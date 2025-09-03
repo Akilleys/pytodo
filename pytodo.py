20d37513f5e90b4e3daf1d424813efc8904d1c93
Akilleys <139573488+Akilleys@users.noreply.github.com>
2025-09-02 21:25:21 -0400

    I touched this, but not alot
    More work in reading to do list objectives

--- pytodo.py
+++ pytodo.py
@@ -1,15 +1,38 @@
 import os
+import shutil
 
-def makeList(lists_dir):
+def getBaseDirectory():
+    return os.path.dirname(__file__)
+
+def getListsFolderLocation():
+    lists_dir = os.path.join(getBaseDirectory(), "lists")
+    return lists_dir
+
+def getArchiveFolderLocation():
+    archive_dir = os.path.join(getBaseDirectory(), "archive")
+    return archive_dir
+
+def makeList():
     print("Input name of new list:")
     listName = input()
-    with open(os.path.join(lists_dir, f"{listName}.txt"), "w") as f:
+    with open(os.path.join(getListsFolderLocation, f"{listName}.txt"), "w") as f:
         f.write("")
         print(listName + " created")
         f.close()
     return
 
-def selectList(listName):
+def selectList():
+    displayLists()
+    print("\nType List Name to Load:\n")
+    selectedList = input()
+    if checkTaskListExists(selectedList):
+        with open(os.path.join(getListsFolderLocation(), f"{selectedList}.txt"),'r') as f:
+            print(f.read())
+
+            print(f"\n{selectedList} loaded: ")
+        return
+    else:
+        print(f"list not found")
     return
 
 def deleteList():
@@ -23,21 +46,47 @@ def startUpChecks():
     return lists_dir
 
 def displayLists():
-    print("\nYour lists: \n")
     list = os.listdir("lists")
     for i in list:
-        print(i[:-4] + "\n")
+        if (i[-4:]) == ".txt":
+            print(i[:-4] + "\n")
     return
+
+def checkForFile(fileLocation):
+    return os.path.exists(fileLocation) and os.path.isfile(fileLocation)
+
+def checkTaskListExists(taskName):
+    fileLocation = getListsFolderLocation() + "/" + taskName + ".txt"
+    return checkForFile(fileLocation)
 
 def archiveList():
+    print("\nSelect list to archive: \n")
+    displayLists()
+    listToArchive = input()
+    if(checkTaskListExists(listToArchive)):
+        print(f"\nList \"{listToArchive}\" located and moved to archived lists.")
+        shutil.move(f"{fileLocation}", f"{getArchiveFolderLocation()}/{listToArchive}.txt")
+    else:
+        print(f"\nList \"{listToArchive}\" selected not located. Please try again")
     return
 
+def readList(fileLocation):
+    with open(os.path.join(lists_dir, f"{listName}.txt"), "r") as f:
+        for x in f:
+            print(x)
+    f.close()
+    return
+
+
 def main():
+    lists_dir = startUpChecks()
+    print("  ____       _____     ____        \n |  _ \\ _   |_   _|__ |  _ \\  ___  \n | |_) | | | || |/ _ \\| | | |/ _ \\ \n |  __/| |_| || | (_) | |_| | (_) |\n |_|    \\__, ||_|\\___/|____/ \\___/ \n        |___/                      ")
+    print("--------------------------------------------\n\n")
     exitBool = False
-    while(not exitBool):
-        lists_dir = startUpChecks()
-        print("  ____       _____     ____        \n |  _ \\ _   |_   _|__ |  _ \\  ___  \n | |_) | | | || |/ _ \\| | | |/ _ \\ \n |  __/| |_| || | (_) | |_| | (_) |\n |_|    \\__, ||_|\\___/|____/ \\___/ \n        |___/                      ")
-        print("--------------------------------------------")
+    currentList = None
+
+
+    while(not exitBool and currentList == None):
         print("1. Make List")
         print("2. Select List")
         print("3. Delete List")
@@ -48,17 +97,21 @@ def main():
             case 1:
                 makeList(lists_dir)
             case 2:
-                displayLists()
+                currentList = selectList()
             case 3:
                 deleteList()   
             case 4:
                 archiveList()
             case 9:
                 exitBool = True
-    
+    if currentList is None:
+        print(f"Viewing {currentList}")
+        choice = int(input())
+        return
 
 class list:
-    def __init__(self, tasks):
+    def __init__(self, name, tasks):
+        name = name
         tasks = []
         
     def __str__(self):
